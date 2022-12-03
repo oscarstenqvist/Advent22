78306ca439cde4d24c286ab88cbe8bfb94ad12dd
Oscar Stenqvist <oscar@Oscars-MBP.lan>
2022-12-03 12:31:47 +0100

    day 3

---
+++ 3/rucksack.py
@@ -0,0 +1,40 @@
+from functools import reduce
+
+
+def main():
+    lines = open("input.txt", "r").read().splitlines()
+    print("Part 1: " + str(reduce(f1, lines)))
+    print("Part 2: " + str(f2(lines)))
+    return
+
+
+def f1(a, b):
+    if isinstance(a, str):
+        return f1(f1(0, a), b)
+    for char1 in b[:(int(len(b)/2))]:
+        for char2 in b[int(len(b)/2):]:
+            if char1 == char2:
+                if ord(char1) < 97:
+                    return a + ord(char1) - 38
+                else:
+                    return a + ord(char1) - 96
+
+
+def f2(lines):
+    if len(lines) < 3:
+        return 0
+    r1 = lines[0]
+    r2 = lines[1]
+    r3 = lines[2]
+    for char1 in r1:
+        for char2 in r2:
+            for char3 in r3:
+                if char1 == char2 == char3:
+                    if ord(char1) < 97:
+                        return ord(char1) - 38 + f2(lines[3:])
+                    else:
+                        return ord(char1) - 96 + f2(lines[3:])
+
+
+if __name__ == "__main__":
+    main()
