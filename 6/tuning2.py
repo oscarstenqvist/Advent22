82ac59535b811ee5b08ffe955e811d3859573b7b
Oscar Stenqvist <oscar@Oscars-MBP.lan>
2022-12-06 22:51:15 +0100

    Solution without using reduce

--- 6/tuning2.py
+++ 6/tuning2.py
@@ -1,26 +1,18 @@
-import re
-
-
 def main():
     line = open("input.txt", "r").read()
-    print("Part 1: ", str(part(line, 4)))
-    print("Part 2: ", str(part(line, 14)))
+    print("Part 1: ", str(tuning(line, 4)))
+    print("Part 2: ", str(tuning(line, 14)))
     return
 
 
-def part(line, ammount):
-    i = 0
-    while i < len(line):
-        j = 0
-        found = False
-        while j < ammount-1:
-            if len(re.findall(line[i + j], line[i:i+ammount])) > 1:
-                found = True
-                break
-            j += 1
-        if not found:
+def tuning(line, ammount):
+    for i in range(len(line)):
+        count = 0
+        substring = line[i:i+ammount]
+        for j in substring:
+            count += substring.count(j)
+        if count == ammount:
             return i + ammount
-        i += 1
 
 
 if __name__ == "__main__":
