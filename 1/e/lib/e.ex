d09d2e0e71bbf2dd234494adc707a7a22dfd6f87
Oscar Stenqvist <oscar@Oscars-MBP.lan>
2022-12-08 23:52:50 +0100

    Start of part 2 elixir

--- 1/e/lib/e.ex
+++ 1/e/lib/e.ex
@@ -1,5 +1,5 @@
 defmodule E do
-  def main do
+  def p1 do
     input = File.read!("input.txt")
     input = String.split(input, "\n")
     input = Enum.chunk_by(input, fn(x) -> x == "" end)
@@ -7,6 +7,19 @@ defmodule E do
     input = Enum.map(input, fn(x) -> Enum.map(x, fn(y) -> String.to_integer(y) end) end)
     input = Enum.map(input, fn(x) -> Enum.sum(x) end)
     input = Enum.max(input)
-    IO.puts(input)
+    IO.puts("Part 1: " <> Integer.to_string(input))
+  end
+  def p2 do
+    input = File.read!("tinput.txt")
+    input = String.split(input, "\n")
+    input = Enum.chunk_by(input, fn(x) -> x == "" end)
+    input = Enum.filter(input, fn(x) -> x != [""] end)
+    input = Enum.map(input, fn(x) -> Enum.map(x, fn(y) -> String.to_integer(y) end) end)
+    input = Enum.map(input, fn(x) -> Enum.sum(x) end)
+    input = Enum.reduce(input, [0,0,0], fn x, acc ->
+      if x > acc[0], do: acc = [x,acc[0],acc[1]], else: if x > acc[1], do: acc = [acc[0], x, acc[1]], else:
+      if x > acc[2], do: acc = [acc[0], acc[1], x]
+    end) #Not working
+    IO.puts("Part 2: " <> Integer.to_string(input))
   end
 end
