02da1ea0383a22e6428495731f9bfd70a0679e4d
Oscar Stenqvist <oscar@Oscars-MBP.lan>
2022-12-09 16:55:00 +0100

    Part 2 done in elixir

--- 1/e/lib/e.ex
+++ 1/e/lib/e.ex
@@ -1,8 +1,7 @@
 defmodule E do
   def p1 do
-    input = prepInput("input.txt")
-    input = Enum.max(input)
-    IO.puts("Part 1: " <> Integer.to_string(input))
+    max = prepInput("input.txt") |> Enum.max() |> Integer.to_string()
+    IO.puts("Part 1: " <> max)
   end
   def p2 do
     input = prepInput("input.txt")
@@ -13,13 +12,10 @@ defmodule E do
     max3 = Enum.max(input)
     IO.puts("Part 2: " <> Integer.to_string(max1+max2+max3))
   end
-
   def prepInput(fileName) do
-    input = File.read!(fileName)
-    input = String.split(input, "\n")
-    input = Enum.chunk_by(input, fn(x) -> x == "" end)
-    input = Enum.filter(input, fn(x) -> x != [""] end)
-    input = Enum.map(input, fn(x) -> Enum.map(x, fn(y) -> String.to_integer(y) end) end)
-    input = Enum.map(input, fn(x) -> Enum.sum(x) end)
+    File.read!(fileName) |> String.split("\n") |> Enum.chunk_by(fn(x) -> x == "" end) |>
+    Enum.filter(fn(x) -> x != [""] end) |>
+    Enum.map(fn(x) -> Enum.map(x, fn(y) -> String.to_integer(y) end) end) |>
+    Enum.map(fn(x) -> Enum.sum(x) end)
   end
 end
