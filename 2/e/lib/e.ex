e727927e7c6bf0a4200d0d0208ec7ea676877e32
Oscar Stenqvist <oscar@Oscars-MBP.lan>
2022-12-09 17:53:43 +0100

    Day 2 elixir

--- 2/e/lib/e.ex
+++ 2/e/lib/e.ex
@@ -6,48 +6,30 @@ defmodule E do
     File.read!("input.txt") |> String.split("\n") |>
     Enum.map(fn x ->
       case x do
-        "A X" ->
-          x = "A Z"
-        "A Y" ->
-          x = "A X"
-        "A Z" ->
-          x = "A Y"
-        "B X" ->
-          x = "B X"
-        "B Y" ->
-          x = "B Y"
-        "B Z" ->
-          x = "B Z"
-        "C X" ->
-          x = "C Y"
-        "C Y" ->
-          x = "C Z"
-        "C Z" ->
-          x = "C X"
+        "A X" -> "A Z"
+        "A Y" -> "A X"
+        "A Z" -> "A Y"
+        "B X" -> "B X"
+        "B Y" -> "B Y"
+        "B Z" -> "B Z"
+        "C X" -> "C Y"
+        "C Y" -> "C Z"
+        "C Z" -> "C X"
       end
     end) |> getScore()
   end
   def getScore(input) do
     Enum.reduce(input, 0, fn x, acc ->
       case x do
-        "A X" ->
-          acc + 1 + 3
-        "A Y" ->
-          acc + 2 + 6
-        "A Z" ->
-          acc + 3 + 0
-        "B X" ->
-          acc + 1 + 0
-        "B Y" ->
-          acc + 2 + 3
-        "B Z" ->
-          acc + 3 + 6
-        "C X" ->
-          acc + 1 + 6
-        "C Y" ->
-          acc + 2 + 0
-        "C Z" ->
-          acc + 3 + 3
+        "A X" -> acc + 1 + 3
+        "A Y" -> acc + 2 + 6
+        "A Z" -> acc + 3 + 0
+        "B X" -> acc + 1 + 0
+        "B Y" -> acc + 2 + 3
+        "B Z" -> acc + 3 + 6
+        "C X" -> acc + 1 + 6
+        "C Y" -> acc + 2 + 0
+        "C Z" -> acc + 3 + 3
       end
     end)
   end
